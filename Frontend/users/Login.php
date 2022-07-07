813fca564b94e326846c70277021a6576dc7a97d
unknown <solannar03@gmail.com>
2022-07-07 21:23:06 +0800

    add

--- Frontend/users/Login.php
+++ Frontend/users/Login.php
@@ -1,42 +1,50 @@
 <html>
     <head>   
-       
+       <?php include('../components/Header.php'); ?>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script> 
     </head>
+
     <body>
-    <form class="row row-cols-lg-auto g-3 align-items-center">
-  <div class="col-12">
-    <label class="visually-hidden" for="inlineFormInputGroupUsername">Username</label>
-    <div class="input-group">
-      <div class="input-group-text">@</div>
-      <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Username">
-    </div>
-  </div>
-
-  <div class="col-12">
-    <label class="visually-hidden" for="inlineFormSelectPref">Preference</label>
-    <select class="form-select" id="inlineFormSelectPref">
-      <option selected>Choose...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </select>
-  </div>
-
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="inlineFormCheck">
-      <label class="form-check-label" for="inlineFormCheck">
-        Remember me
-      </label>
-    </div>
-  </div>
-
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </div>
-</form>
+      <div class="container" style="padding: 65px;">
+        <div class="col mx-auto d-flex justify-content-center">
+          
+          <div class="card shadow lg" style="width: 50rem; padding: 55px;">
+            <div class="card-body">
+              <h2 class="card-title col-md-auto">Login</h2>
+              <form class="needs-validation" >
+                <div class="col-md-auto">
+                  <label for="validationTooltipUsernamePrepend" class="form-label">Username</label>
+                    <div class="input-group">
+                  <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
+                <input type="text" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>
+                <!-- <div class="invalid-tooltip">
+        Please choose a unique and valid username.
+      </div> -->
+              </div>
+            </div>
+              <div class="col-md-auto">
+                <label for="validationDefault03" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="validationDefault03" required>
+               </div>
+                <div class="col-md-auto">
+                  <div class="form-check">
+                   <input class="form-check-input" type="checkbox" value="" id="remember me">
+                  <label class="form-check-label">
+                        Remember Me
+                  </label>
+              </div>
+            </div>
+              <div class="col-md-auto" style="padding: 5px;">
+                <button class="btn btn-primary btn-lg" type="submit" >Login</button>
+             </div>
+            </form>
+           </div>
+          </div>
+        </div>
+      </div>
+    
     </body>
     
 </html>
